e2b2b7801a96d90fe1161e0871bf755b6f111c82
James Mertens <44683915+jammer42@users.noreply.github.com>
2021-05-25 18:12:34 +0100

    Inc version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="helloworld-jammer",
-    version="0.0.1",
+    version="0.0.2",
     description="Returns 'Hello, World!'",
     long_description=long_description,
     long_description_content_type="text/markdown",
